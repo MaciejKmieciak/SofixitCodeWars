83c6a7928309f9b65f74aa61015ce13abd84c82a
MaciejKmieciak <80123758+MaciejKmieciak@users.noreply.github.com>
2023-10-17 23:18:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from material import material
+
+test_cases = [
+    [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
+    [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2],
+    [4, 2, 0, 3, 2, 5],
+    [6, 4, 2, 0, 3, 2, 0, 3, 1, 4, 5, 3, 2, 7, 5, 3, 0, 1, 2, 1, 3, 4, 6, 8, 1, 3],
+    [6, 2, 1, 1, 8, 0, 5, 5, 0, 1, 8, 9, 6, 9, 4, 8, 0, 0],
+    [2, 0, 0, 2, 0, 1, 0],
+    [8, 0, 2, 1, 2, 0, 8, 1],
+    [2, 0, 1, 3, 2, 3],
+    [0, 1, 0, 0],
+    [3, 3, 3],
+    [2],
+    [0, 0, 0],
+    [0],
+    [],
+    'xyz',
+    [1, 2, 'abc'],
+    [1, 0, 2, -3]
+]
+
+for test_case in test_cases:
+    print("Test case:", test_case)
+    try:
+        print("Result:", material(test_case))
+    except ValueError as my_value_error:
+        print(my_value_error)
+    print()
---
+++ material.py
@@ -0,0 +1,63 @@
+def material(spaceship):
+    # Check whether given argument is a list
+    if isinstance(spaceship, list):
+
+        # Check whether all elements of the list are integers
+        if not all(isinstance(element, int) for element in spaceship):
+            # Raise exception if not
+            raise ValueError('material() error: not all elements of the list are integers')
+    else:
+        # Raise exception if not a list
+        raise ValueError('material() error: argument is not a list')
+
+    # Check whether all elements of the list are non-negative
+    if min(spaceship) < 0:
+        raise ValueError('material() error: elements of the list cannot be negative')
+
+    # Initialize material amount - this is the number to be returned
+    material_amount = 0
+
+    len_spaceship = len(spaceship)
+
+    # In case the list is empty, no material can be placed
+    if len_spaceship == 0:
+        return material_amount
+
+    max_level = max(spaceship)
+
+    # For each horizontal level of given spaceship,
+    # up to the highest level which still has boxes
+    for current_level in range(1, max_level + 1):
+
+        left_index_limit = None
+        right_index_limit = None
+
+        # Find the left index limit for current level
+        # i.e. the first element in the list,
+        # which is bigger than or equal to the current level.
+        for i in range(len_spaceship):
+            if spaceship[i] >= current_level:
+                left_index_limit = i
+                break
+
+        # In case no such index is found,
+        # there is no more space to fill.
+        if left_index_limit is None:
+            return material_amount
+
+        # Similarly for the right index limit
+        for i in range(len_spaceship):
+            if spaceship[len_spaceship - (i + 1)] >= current_level:
+                right_index_limit = len_spaceship - (i + 1)
+                break
+
+        # Fill all the empty space in between with material
+        # on the current level, except the space taken by boxes.
+        # In case there is no such range,
+        # the loop will not be executed even once.
+        for i in range(left_index_limit + 1, right_index_limit):
+            if spaceship[i] < current_level:  # Check if the space is taken by a box.
+                material_amount += 1
+
+    # At this point all possible levels to fill have been covered
+    return material_amount
